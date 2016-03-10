291b1c73a02d0f8ce4931d3a40a1e2f6dc6f7f2d
ubuntu <1124ritesh@gmail.com>
2016-03-10 15:13:00 +0530

    permissions

---
+++ backend/views/auth-item/list.php
@@ -0,0 +1,17 @@
+
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\AuthItemSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Auth Items';
+$this->params['breadcrumbs'][] = $this->title;
+$inc=1;
+?>
+
+   
+
