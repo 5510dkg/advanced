6663a7db7701ff8732b2d70018434e9974fa4169
ubuntu <1124ritesh@gmail.com>
2016-03-15 13:52:38 +0530

    headers

--- backend/modules/settings/views/payment-mode/index.php
+++ backend/modules/settings/views/payment-mode/index.php
@@ -16,6 +16,12 @@ $this->params['breadcrumbs'][] = $this->title;
 CrudAsset::register($this);
 
 ?>
+
+<div class="row">
+    <div class="page-header">
+        <h1>Payment Mode</h1>
+    </div>
+</div>
 <div class="payment-mode-index">
     <div id="ajaxCrudDatatable">
         <?=GridView::widget([
