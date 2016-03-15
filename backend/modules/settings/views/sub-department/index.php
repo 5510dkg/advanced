a4af10a03b77f84cf3e7f8989cbda71c81c3f501
ubuntu <1124ritesh@gmail.com>
2016-03-15 13:53:51 +0530

    headers

--- backend/modules/settings/views/sub-department/index.php
+++ backend/modules/settings/views/sub-department/index.php
@@ -16,6 +16,12 @@ $this->params['breadcrumbs'][] = $this->title;
 CrudAsset::register($this);
 
 ?>
+
+<div class="row">
+    <div class="page-header">
+        <h1>Sub Department</h1>
+    </div>
+</div>
 <div class="sub-department-index">
     <div id="ajaxCrudDatatable">
         <?=GridView::widget([
--- backend/modules/settings/views/weight/index.php
+++ backend/modules/settings/views/weight/index.php
@@ -16,6 +16,12 @@ $this->params['breadcrumbs'][] = $this->title;
 CrudAsset::register($this);
 
 ?>
+
+<div class="row">
+    <div class="page-header">
+        <h1>Weight</h1>
+    </div>
+</div>
 <div class="weight-index">
     <div id="ajaxCrudDatatable">
         <?=GridView::widget([
