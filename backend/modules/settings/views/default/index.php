00087f744f253ed5f11087528b4481fddcb81003
ubuntu <1124ritesh@gmail.com>
2016-03-15 15:38:58 +0530

    headers

--- backend/modules/settings/views/default/index.php
+++ backend/modules/settings/views/default/index.php
@@ -1,4 +1,10 @@
 <?php
+$this->title='Home';
+$this->registerJsFile('@web/bower_components/jquery/dist/jquery.min.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
+$this->registerJsFile('@web/bower_components/bootstrap/dist/js/bootstrap.min.js', ['depends' => [\yii\bootstrap\BootstrapAsset::className()]]);
+
+?>
+<?php
 use yii\helpers\Html;
 
 ?> 
