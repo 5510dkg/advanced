714f0d3757d71a896d97cc4592fce64f24d4170c
ubuntu <1124ritesh@gmail.com>
2016-03-17 13:12:28 +0530

    print

--- backend/modules/circulation/views/billing/welcome.php
+++ backend/modules/circulation/views/billing/welcome.php
@@ -24,7 +24,42 @@ use dosamigos\datepicker\DatePicker;
             'minViewMode'=> "months"
         ]
 ]); ?>
+    <br/>
+    <h3>No of. Special Editions In Selected Month</h3>
+    <select class="form-control" id="optionselect" required="required">
+        <option value="0">0</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        
+    </select>
+    <div class="one">
+        
+            <?= DatePicker::widget([
+         'name' => 'date1',
+         //'value' => '02-16-2012',
+         'template' => '{addon}{input}',
+             'clientOptions' => [
+                 'autoclose' => true,
+                 'format'=> "yyyy-mm-dd",
+             ]
+     ]); ?> 
+       
+    </div>
+    <div class="two">
+        
+    </div>
+    <div class="three">
     
+    </div>
+    <div class="four">
+        
+    </div>
+    <div class="five">
+        
+    </div>
     
     
     
