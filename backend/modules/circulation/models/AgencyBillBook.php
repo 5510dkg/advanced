1ddb304c1573b647836ebbec8f617e3de786e3e4
ubuntu <1124ritesh@gmail.com>
2016-03-18 12:13:41 +0530

    billing display

--- backend/modules/circulation/models/AgencyBillBook.php
+++ backend/modules/circulation/models/AgencyBillBook.php
@@ -284,7 +284,7 @@ class AgencyBillBook extends \yii\db\ActiveRecord
     
     
     public function getDetails($month,$agency_id){
-         $query = (new \yii\db\Query())->select(['DATE_FORMAT(issue_date,"D-M-Y") as date,agency.id as agencyid,agency.name as name,agency_bill_book.pjy as pjy,agency_bill_book.org as org,total_copies,total_price,discounted_amt,final_total'])->from('agency_bill_book')->innerJoin('agency','agency.id=agency_bill_book.agency_id')->where(['DATE_FORMAT(issue_date,"%Y%m")="'.$month.'"','agency_id'=>$agency_id])->groupBy('agency_id');
+         $query = (new \yii\db\Query())->select(['DATE_FORMAT(issue_date,"D-M-Y") as date,agency.id as agencyid,agency.name as name,agency_bill_book.pjy as pjy,agency_bill_book.org as org,total_copies,total_price,discounted_amt,final_total'])->from('agency_bill_book')->innerJoin('agency','agency.id=agency_bill_book.agency_id')->where(['DATE_FORMAT(issue_date,"%Y%m")'=>$month,'agency_id'=>$agency_id])->groupBy('agency_id');
              $command = $query->createCommand();
              return $data = $command->queryAll();
             // $titles = '';
