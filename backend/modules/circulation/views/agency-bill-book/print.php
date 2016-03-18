133e069447f9acaf13e6c96eb4c4697c368f4f19
ubuntu <1124ritesh@gmail.com>
2016-03-18 13:17:02 +0530

    billing display

--- backend/modules/circulation/views/agency-bill-book/print.php
+++ backend/modules/circulation/views/agency-bill-book/print.php
@@ -6,12 +6,13 @@ $agency_id=Yii::$app->request->get('agency_id');
 $month=Yii::$app->request->get('month');
 $agency=new AgencyBillBook();
 $data=$agency->getPrintdetails($month, $agency_id);
-print_r($data);
+//print_r($data);
 
 $i=1;$r=1;?>
 
 
 	<div style="margin-left: 25px;margin-bottom: 40px;margin-right: -90px; width: 490px;float: left; height: 300px;">
+            <img src="logoa.png" width="100%">
 	
 	</div>
 
