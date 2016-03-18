78817a6a1d925161e1361e8cdcf2e52e7e1dbb20
ubuntu <1124ritesh@gmail.com>
2016-03-18 13:33:43 +0530

    billing display

--- backend/modules/circulation/views/agency-bill-book/print.php
+++ backend/modules/circulation/views/agency-bill-book/print.php
@@ -11,8 +11,33 @@ $data=$agency->getPrintdetails($month, $agency_id);
 $i=1;$r=1;?>
 
 
-	<div style="margin-left: 25px;margin-bottom: 40px;margin-right: -90px; width: 490px;float: left; height: 300px;">
-            <img src="logoa.png">
+	<div style="margin-left: 25px;margin-bottom: 40px;margin-right: -90px; width: 100%;float: left; height: 300px;">
+            <div style="height: 130px; width: 100%; border: 1px solid black;">
+                hii
+            </div>
+            <div style="width: 100%;height: 15px;">
+             Code:&nbsp;&nbsp;&nbsp;&nbsp;<?=$data[0]['account_id']?> 
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+             Bill Number: 24
+            </div>
+            <div style="width: 90%;margin-left: 120px; float: left; border:1px solid black;">
+                <br/>
+                <?=$data[0]['name'];?><br/>
+                <?=$data[0]['add_house_no']?><br/>
+                <?=$data[0]['add_street_address']?><br/>
+                <?=$data[0]['add_p_office']?><br/>
+                <?=$data[0]['add_pincode']?><br/>
+                
+            </div>
             
 	
 	</div>
