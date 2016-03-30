0b79fadca3e33ce7c95182745953301f98eaf71b
ubuntu <1124ritesh@gmail.com>
2016-03-30 14:19:56 +0530

    billing display

--- backend/modules/circulation/views/billing/view.php
+++ backend/modules/circulation/views/billing/view.php
@@ -1,7 +1,7 @@
 <?php
 
 use yii\widgets\DetailView;
-
+$inc=1;
 /* @var $this yii\web\View */
 /* @var $model backend\modules\circulation\models\Billing */
 ?>
@@ -10,12 +10,43 @@ use yii\widgets\DetailView;
         <h1>Billing Detail</h1>
     </div>
 </div>
+<?php print_r($model); ?>
 <div class="billing-view">
     <div class="row">
         <table class="table table-striped table-bordered detail-view">
             <tbody>
-               
+                <tr>
+                    <td>
+                        #
+                    </td>
+                    <td>
+                        Issue date
+                    </td>
+                    <td>
+                        PJY
+                    </td>
+                    <td>
+                        ORG
+                    </td>
+                    <td>
+                        DISCOUNT
+                    </td>
+                   
+                </tr>
+               <?php foreach($model as $row): ?> 
                 
+                <tr>
+                    <td><?= $inc;?></td>
+                    <td><?=$row['date']?></td>
+                    <td><?=$row['pjy']?></td>
+                    <td><?=$row['org']?></td>
+                </tr>
+                
+                
+                
+                <?php
+                $inc++;
+                endforeach; ?>
             </tbody>
         </table>  
     </div>
