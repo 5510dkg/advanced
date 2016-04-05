cd7ccb818d9bd043a25c3ba4f5b67b406edf5987
ubuntu <1124ritesh@gmail.com>
2016-04-05 16:33:04 +0530

    work on ui

--- backend/views/layouts/header.php
+++ backend/views/layouts/header.php
@@ -230,7 +230,7 @@ use yii\helpers\Html;
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <img src="<?= $directoryAsset ?>/img/user.jpg" class="user-image" alt="User Image"/>
-                        <span class="hidden-xs">Alexander Pierce</span>
+                        <span class="hidden-xs"><?=  strtoupper(Yii::$app->user->identity->name)?> </span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- User image -->
@@ -239,22 +239,11 @@ use yii\helpers\Html;
                                  alt="User Image"/>
 
                             <p>
-                                Alexander Pierce - Web Developer
-                                <small>Member since Nov. 2012</small>
+                                <?=Yii::$app->user->identity->name?> 
                             </p>
                         </li>
                         <!-- Menu Body -->
-                        <li class="user-body">
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Followers</a>
-                            </div>
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Sales</a>
-                            </div>
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Friends</a>
-                            </div>
-                        </li>
+                        
                         <!-- Menu Footer-->
                         <li class="user-footer">
                             <div class="pull-left">
