69b37c98535f8e06d46e737c2f0097972abd729c
ubuntu <1124ritesh@gmail.com>
2016-04-05 16:03:58 +0530

    work on ui

--- backend/modules/user/views/default/dashboard.php
+++ backend/modules/user/views/default/dashboard.php
@@ -1,7 +1,5 @@
 <?php
 $this->title='Home';
-$this->registerJsFile('@web/bower_components/jquery/dist/jquery.min.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
-$this->registerJsFile('@web/bower_components/bootstrap/dist/js/bootstrap.min.js', ['depends' => [\yii\bootstrap\BootstrapAsset::className()]]);
 
 ?>
 <?php
