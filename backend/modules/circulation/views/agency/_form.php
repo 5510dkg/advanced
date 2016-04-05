4ad733b6a8f6b881531096d0a16939931c84f9ab
ubuntu <1124ritesh@gmail.com>
2016-04-05 16:47:33 +0530

    work on ui

--- backend/modules/circulation/views/agency/_form.php
+++ backend/modules/circulation/views/agency/_form.php
@@ -11,20 +11,6 @@ use dosamigos\datepicker\DatePicker;
 use kartik\typeahead\Typeahead;
 
 
-
-//$data = [
-//    'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 
-//    'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-//    'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-//    'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-//    'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-//    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-//    'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-//    'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-//    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-//];
-
-
 /* @var $this yii\web\View */
 /* @var $model backend\modules\circulation\models\Agency */
 /* @var $form yii\widgets\ActiveForm */
