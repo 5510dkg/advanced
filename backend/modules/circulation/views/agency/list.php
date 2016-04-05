5fa0627e4462e9568aa0194c6f257acaec888a41
ubuntu <1124ritesh@gmail.com>
2016-04-05 15:36:12 +0530

    work on ui

--- backend/modules/circulation/views/agency/list.php
+++ backend/modules/circulation/views/agency/list.php
@@ -9,20 +9,20 @@ $this->title='Agency Management';?>
 </div>
 <div class="row">
 <div class="col-sm-3">
-     <?=Html::a('Update Address',['/circulation/agency',['q'=>'add']],['class'=>'btn btn-success btn-lg btn-block'])?>  
+     <?=Html::a('Update Address',['/circulation/agency/searchaddress',['q'=>'add']],['class'=>'btn btn-success btn-lg btn-block'])?>  
 </div>
 <div class="col-sm-3">
-     <?=Html::a('New Agency',['/circulation/agency',['q'=>'create']],['class'=>'btn btn-success btn-lg btn-block'])?>  
+     <?=Html::a('New Agency',['/circulation/agency/create',['q'=>'create']],['class'=>'btn btn-success btn-lg btn-block'])?>  
 </div>
 <div class="col-sm-3">
-     <?=Html::a('Update Copies',['/circulation/agency',['q'=>'copy']],['class'=>'btn btn-success btn-lg btn-block'])?>  
+     <?=Html::a('Update Copies',['/circulation/agency/searchcopies',['q'=>'copy']],['class'=>'btn btn-success btn-lg btn-block'])?>  
 </div>
     <div class="col-sm-3">
-     <?=Html::a('Deactivate Agecny',['/circulation/agency',['q'=>'deactive']],['class'=>'btn btn-warning btn-lg btn-block'])?>  
+     <?=Html::a('Deactivate Agecny',['/circulation/agency/searchdeactivate',['q'=>'deactive']],['class'=>'btn btn-warning btn-lg btn-block'])?>  
 </div>
 </div>
 <div class="row" style="margin-top: 10px;">
 <div class="col-sm-3">
-     <?=Html::a('Update Delivery Method',['/circulation/agency',['q'=>'delivery']],['class'=>'btn btn-success btn-lg btn-block'])?>  
+     <?=Html::a('Update Delivery Method',['/circulation/agency/searchdelivery',['q'=>'delivery']],['class'=>'btn btn-success btn-lg btn-block'])?>  
 </div>
 </div>
