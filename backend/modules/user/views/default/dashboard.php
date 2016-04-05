3283bd427f059dafde48a529be0d7edabf836f57
ubuntu <1124ritesh@gmail.com>
2016-04-05 16:38:39 +0530

    work on ui

--- backend/modules/user/views/default/dashboard.php
+++ backend/modules/user/views/default/dashboard.php
@@ -8,46 +8,18 @@ use yii\helpers\Html;
 ?> 
 <?php  if(Yii::$app->user->can('generate-labels')){ ?>
 <div class="col-lg-4">
-                    <div class="panel panel-success">
-                        <div class="panel-heading">
-                            Railway Labels
-                        </div>
-                        <div class="panel-body">
-                        <?=Html::a('Railway Labels',['/circulation/railway-post-data'],['class'=>'btn btn-success btn-lg btn-block'])?>  
-                        </div>
-                        <div class="panel-footer">
-                           Railway Labels
-                        </div>
-                    </div>
+                        <?=Html::a('Railway Labels',['/circulation/railway-post-data'],['class'=>'btn btn-success btn-lg btn-block'])?> 
                 </div> 
 <?php } ?>
 <?php  if(Yii::$app->user->can('generate-labels')){ ?>
 <div class="col-lg-4">
-                    <div class="panel panel-success">
-                        <div class="panel-heading">
-                            Ordinary Post Labels
-                        </div>
-                        <div class="panel-body">
                         <?=Html::a('Ordinary Post Labels',['/circulation/ordinary-post-data'],['class'=>'btn btn-success btn-lg btn-block'])?>  
-                        </div>
-                        <div class="panel-footer">
-                           Ordinary Post Labels
-                        </div>
-                    </div>
+                        
                 </div> 
 <?php } ?>
 <?php  if(Yii::$app->user->can('generate-labels')){ ?>
 <div class="col-lg-4">
-                    <div class="panel panel-success">
-                        <div class="panel-heading">
-                            Registered Post Labels
-                        </div>
-                        <div class="panel-body">
                         <?=Html::a('Registered Post Labels',['/circulation/registered-post-data'],['class'=>'btn btn-success btn-lg btn-block'])?>  
-                        </div>
-                        <div class="panel-footer">
-                           Registered Post Labels
-                        </div>
-                    </div>
+                        
                 </div> 
 <?php } ?>
