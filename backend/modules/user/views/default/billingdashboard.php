b4e7a530402335a89c1ac9dea8e05f52680664be
ubuntu <1124ritesh@gmail.com>
2016-04-05 16:37:48 +0530

    work on ui

--- backend/modules/user/views/default/billingdashboard.php
+++ backend/modules/user/views/default/billingdashboard.php
@@ -8,62 +8,27 @@ use yii\helpers\Html;
 ?> 
 <?php  if(Yii::$app->user->can('generate-bill')){ ?>
 <div class="col-lg-4">
-                    <div class="panel panel-success">
-                        <div class="panel-heading">
-                            Railway Labels
-                        </div>
-                        <div class="panel-body">
+                   
                         <?=Html::a('Generate Bill',['/circulation/billing'],['class'=>'btn btn-success btn-lg btn-block'])?>  
-                        </div>
-                        <div class="panel-footer">
-                           Railway Labels
-                        </div>
-                    </div>
+                        
                 </div> 
 <?php } ?>
 <?php  if(Yii::$app->user->can('upload-credit-note')){ ?>
 <div class="col-lg-4">
-                    <div class="panel panel-success">
-                        <div class="panel-heading">
-                            Credit Note
-                        </div>
-                        <div class="panel-body">
                         <?=Html::a('Credit Note',['/circulation/agency-credit-note/list'],['class'=>'btn btn-success btn-lg btn-block'])?>  
-                        </div>
-                        <div class="panel-footer">
-                           Credit Note
-                        </div>
-                    </div>
+                        
                 </div> 
 <?php } ?>
 
 <?php  if(Yii::$app->user->can('upload-receipt')){ ?>
 <div class="col-lg-4">
-                    <div class="panel panel-success">
-                        <div class="panel-heading">
-                            Receipt
-                        </div>
-                        <div class="panel-body">
                         <?=Html::a('Agency Receipts',['/circulation/agency-receipt/list'],['class'=>'btn btn-success btn-lg btn-block'])?>  
-                        </div>
-                        <div class="panel-footer">
-                          Receipt
-                        </div>
-                    </div>
+                        
                 </div> 
 <?php } ?>
 <?php  if(Yii::$app->user->can('search-bill')){ ?>
 <div class="col-lg-4">
-                    <div class="panel panel-success">
-                        <div class="panel-heading">
-                            Receipt
-                        </div>
-                        <div class="panel-body">
                         <?=Html::a('Search Bill',['/circulation/billing/search'],['class'=>'btn btn-success btn-lg btn-block'])?>  
-                        </div>
-                        <div class="panel-footer">
-                          Receipt
-                        </div>
-                    </div>
+                        
                 </div> 
 <?php } ?>
