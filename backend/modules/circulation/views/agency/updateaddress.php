5d5ee9ccaa46598cd98c0376f90606e21dcf8a5f
ubuntu <1124ritesh@gmail.com>
2016-04-05 15:31:49 +0530

    work on ui

--- backend/modules/circulation/views/agency/updateaddress.php
+++ backend/modules/circulation/views/agency/updateaddress.php
@@ -56,7 +56,7 @@ $this->title='Agency|Search';
 
 <div class=" box box-success">
     <div class="box-header with-border">
-        <h3 class="box-title">Search Agency</h3>
+        <h3 class="box-title">Agency Listings</h3>
         <div class="box-tools pull-right">
             <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
         </div>
