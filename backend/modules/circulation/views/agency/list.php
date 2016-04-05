4f33361865f1b63f719b5eb94def8e6c0d3665f6
ubuntu <1124ritesh@gmail.com>
2016-04-05 15:37:18 +0530

    work on ui

--- backend/modules/circulation/views/agency/list.php
+++ backend/modules/circulation/views/agency/list.php
@@ -2,11 +2,7 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 $this->title='Agency Management';?>
-<div class="row">
-    <div class="page-header">
-        <h1>Agency Management</h1>
-    </div>
-</div>
+
 <div class="row">
 <div class="col-sm-3">
      <?=Html::a('Update Address',['/circulation/agency/searchaddress',['q'=>'add']],['class'=>'btn btn-success btn-lg btn-block'])?>  
