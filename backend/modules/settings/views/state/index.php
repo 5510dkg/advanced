305c865f6cda6f823ae20b2bb1f2138684bdd6ec
ubuntu <1124ritesh@gmail.com>
2016-04-05 17:01:11 +0530

    work on ui

--- backend/modules/settings/views/state/index.php
+++ backend/modules/settings/views/state/index.php
@@ -17,11 +17,6 @@ CrudAsset::register($this);
 
 ?>
 
-<div class="row">
-    <div class="page-header">
-        <h1>State</h1>
-    </div>
-</div>
 <div class="state-index">
     <div id="ajaxCrudDatatable">
         <?=GridView::widget([
