ad4379bcd07cbe85b295e4db7aee5a2c503d03d3
ubuntu <1124ritesh@gmail.com>
2016-04-06 18:01:44 +0530

    work on ui

--- backend/modules/admin/views/users/resetform.php
+++ backend/modules/admin/views/users/resetform.php
@@ -2,12 +2,6 @@
 use yii\widgets\ActiveForm;
 use yii\helpers\Html;
 ?>
-<div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Reset Password </h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
 <div class="col-lg-4 col-sm-offset-2">
 <?php $form = ActiveForm::begin([
     'method' => 'post',
