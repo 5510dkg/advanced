cc66e322f2e6857827560c1e7991f139c7d2d3c8
ubuntu <1124ritesh@gmail.com>
2016-04-06 10:07:51 +0530

    work on ui

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -47,7 +47,7 @@
                     ['label' => 'Database','icon' => 'fa fa-edit', 'url' => ['/backuprestore'], 'visible' => Yii::$app->user->can('access-mgmt')],
                      ['label' => 'Agency Management','icon' => 'fa fa-edit', 'url' => ['/circulation/agency/list'], 'visible' => Yii::$app->user->can('create-agency')],
                      ['label' => 'Labels Management','icon' => 'fa fa-file-pdf-o', 'url' => ['/user/default/dashboard'], 'visible' => Yii::$app->user->can('generate-labels')],
-                     ['label' => 'Billing','icon' => 'fa fa-edit', 'url' => ['/user/default/billingdashboard'], 'visible' => Yii::$app->user->can('generate-bill')],
+                     ['label' => 'Billing','icon' => 'fa fa-inr', 'url' => ['/user/default/billingdashboard'], 'visible' => Yii::$app->user->can('generate-bill')],
                     //agency management menu starts here
 //                    [
 //                        'label' => 'Agency Management',
