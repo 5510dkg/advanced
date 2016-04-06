100633d536d405cdc173a6ccf8ccf0597bf73488
ubuntu <1124ritesh@gmail.com>
2016-04-06 11:13:12 +0530

    work on ui

--- backend/modules/circulation/models/Agency.php
+++ backend/modules/circulation/models/Agency.php
@@ -69,7 +69,7 @@ class Agency extends \yii\db\ActiveRecord
     {
         return [
             [['name', 'email', 'landline_no','agency_type','mobile_no', 'mail_house_no', 'mail_street_address', 'mail_p_office','add_house_no','add_street_address','add_p_office','add_pincode','add_country_id', 'add_state_id', 'add_district_id' ,'mail_country_id', 'mail_state_id', 'mail_district_id', 'mail_pincode','commission','agency_type','route_id'], 'required'],
-            [['name','vehicle_id' ,'status','agency_type','commission','comment','issue_start_date','mail_street_address', 'add_street_address','agency_combined_id','train_no','train_name','source'], 'string'],
+            [['name','billing_id','vehicle_id' ,'status','agency_type','commission','comment','issue_start_date','mail_street_address', 'add_street_address','agency_combined_id','train_no','train_name','source'], 'string'],
             [['route_id',  'landline_no', 'mobile_no', 'mail_country_id', 'mail_state_id', 'mail_district_id', 'mail_pincode', 'panchjanya', 'organiser', 'add_country_id', 'add_state_id', 'add_district_id', 'add_pincode'], 'integer'],
             [['security_amt'], 'number'],
 
@@ -129,6 +129,7 @@ class Agency extends \yii\db\ActiveRecord
             'train_no'=>'Train Number',
             'source'=>'Source',
             'train_name'=>'Train Name',
+            'billing_id'=>'Billing Id',
         ];
     }
 
@@ -158,9 +159,11 @@ class Agency extends \yii\db\ActiveRecord
             if($atyp=='Single'){
                 $atype='S';
             }else{$atype='C';}
+            
             $mystate=mb_substr($st, 0, 2);
             $num=str_pad($num, 4, '0', STR_PAD_LEFT);
             $this->account_id=$mystate.'|'.$atype.'|'.$num;
+            $this->billing_id='';
             
             
         return true;
