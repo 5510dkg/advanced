1491a1e16bb5bfb5ab927119afd30f6fc6a47651
ubuntu <1124ritesh@gmail.com>
2016-04-06 10:29:20 +0530

    work on ui

--- backend/modules/circulation/views/agency/view.php
+++ backend/modules/circulation/views/agency/view.php
@@ -6,6 +6,7 @@ use yii\widgets\DetailView;
 /* @var $model backend\modules\circulation\models\Agency */
 ?>
 <div class="agency-view">
+    <div class="row">
     <div class="col-md-6">    
  <div class=" box box-default">
     <div class="box-header with-border">
@@ -18,17 +19,17 @@ use yii\widgets\DetailView;
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
+          //  'id',
             'name:ntext',
             'account_id',
             'route_id',
             'vehicle_id',
             'reference',
-//            'email:email',
-//            'landline_no',
-//            'mobile_no',
-//            'status',
-//            'security_amt',
+            'email:email',
+            'landline_no',
+            'mobile_no',
+            'status',
+            'security_amt',
 //            'address_status',
 //            'mail_house_no',
 //            'mail_street_address:ntext',
@@ -37,8 +38,8 @@ use yii\widgets\DetailView;
 //            'mail_state_id',
 //            'mail_district_id',
 //            'mail_pincode',
-//            'panchjanya',
-//            'organiser',
+            'panchjanya',
+            'organiser',
 //            'add_house_no',
 //            'add_street_address:ntext',
 //            'add_p_office',
@@ -63,12 +64,59 @@ use yii\widgets\DetailView;
             <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
-            'name:ntext',
-            'account_id',
-            'route_id',
-            'vehicle_id',
-            'reference',
+//            'id',
+//            'name:ntext',
+//            'account_id',
+//            'route_id',
+//            'vehicle_id',
+//            'reference',
+//            'email:email',
+//            'landline_no',
+//            'mobile_no',
+//            'status',
+//            'security_amt',
+//            'address_status',
+            'mail_house_no',
+            'mail_street_address:ntext',
+            'mail_p_office',
+            'mail_country_id',
+            'mail_state_id',
+            'mail_district_id',
+            'mail_pincode',
+//            'panchjanya',
+//            'organiser',
+//            'add_house_no',
+//            'add_street_address:ntext',
+//            'add_p_office',
+//            'add_country_id',
+//            'add_state_id',
+//            'add_district_id',
+//            'add_pincode',
+        ],
+    ]) ?>
+        </div>
+    </div>   
+    </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="box box-danger">
+         <div class="box-header with-border">
+        <h3 class="box-title">Billing Address Details</h3>
+        <div class="box-tools pull-right">
+            <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+        </div>
+    </div>
+                <div class="box-body">
+                   <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+//            'id',
+//            'name:ntext',
+//            'account_id',
+//            'route_id',
+//            'vehicle_id',
+//            'reference',
 //            'email:email',
 //            'landline_no',
 //            'mobile_no',
@@ -84,17 +132,17 @@ use yii\widgets\DetailView;
 //            'mail_pincode',
 //            'panchjanya',
 //            'organiser',
-//            'add_house_no',
-//            'add_street_address:ntext',
-//            'add_p_office',
-//            'add_country_id',
-//            'add_state_id',
-//            'add_district_id',
-//            'add_pincode',
+            'add_house_no',
+            'add_street_address:ntext',
+            'add_p_office',
+            'add_country_id',
+            'add_state_id',
+            'add_district_id',
+            'add_pincode',
         ],
-    ]) ?>
+    ]) ?> 
+                </div>
+            </div>        
         </div>
-    </div>   
     </div>
-
 </div>
