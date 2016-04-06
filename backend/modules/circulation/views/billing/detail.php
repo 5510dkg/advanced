23bcb20359c6e74e5a0953a420152999677c74d8
ubuntu <1124ritesh@gmail.com>
2016-04-06 16:33:00 +0530

    work on ui

--- backend/modules/circulation/views/billing/detail.php
+++ backend/modules/circulation/views/billing/detail.php
@@ -11,14 +11,16 @@ $this->title='Bill|Search';
 //print_r($_POST);exit;
 
 ?>
-<div class="row">
-    <div class="page-header">
-        <h1>Agency Billing |Search</h1>
+<div class="box box-primary">
+        <div class="box-header with-border">
+        <h3 class="box-title">Bill Search</h3>
+        <div class="box-tools pull-right">
+            <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+        </div>
     </div>
-</div>
-
-<div class="row">
-    <div class="form-vertical">
+    <div class="box-body">
+        <div class="row">
+            <div class="col-md-3">
    <?php $form = ActiveForm::begin(); ?>
         <?= $form->field($model, 'month')->widget(
     DatePicker::className(), [  
@@ -28,8 +30,8 @@ $this->title='Bill|Search';
                                 'viewMode'=> "months", 
                                 'minViewMode'=> "months",
         ]
-]);?>
- 
+        ]);?></div>
+            <div class="col-md-3">
         <?= $form->field($model, 'name')->widget(Typeahead::classname(), [
                     'dataset' => [
                         [
@@ -40,16 +42,32 @@ $this->title='Bill|Search';
                     'pluginOptions' => ['highlight' => true],
                     'options' => ['placeholder' => 'Filter as you type ...'],
 ]); ?>
-        <?= $form->field($model, 'account_id') ?>
+            </div>
+            <div class="col-md-3">
+                <?= $form->field($model, 'account_id') ?>
+            </div>
+            <div class="col-md-3">
         <?= $form->field($model, 'mail_pincode')->label('Pin Code') ?>
+            </div>
+        </div>
+        
  
         <div class="form-group">
             <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
         </div>
     <?php ActiveForm::end(); ?>
-    </div>
+   
 </div>
-<div class="row">
+    </div>
+<div class="box box-success">
+        <div class="box-header with-border">
+        <h3 class="box-title">Search Results</h3>
+        <div class="box-tools pull-right">
+            <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+        </div>
+    </div>
+    <div class="box-body">
+        
     <?= \yii\grid\GridView::widget([
     'dataProvider' => $list,
      'columns' => [
@@ -89,4 +107,5 @@ $this->title='Bill|Search';
    
 ]);
 ?>
-</div>
+
+    </div></div>
