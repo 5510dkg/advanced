6f1f76c0354edb4cee51cbb0c024cf29d4913b1a
ubuntu <1124ritesh@gmail.com>
2016-04-06 17:32:06 +0530

    work on ui

--- backend/modules/circulation/views/agency-credit-note/_form.php
+++ backend/modules/circulation/views/agency-credit-note/_form.php
@@ -63,7 +63,7 @@ use dosamigos\datepicker\DatePicker;
     <div class="row">
     <div class="col-md-4">
 
-    <?= $form->field($model, 'return_type')->dropDownList([ 'cut' => 'cut','RSL' => 'RSL','copy'=>'copy' ]) ?>
+    <?= $form->field($model, 'return_type')->dropDownList([ 'CUT' => 'CUT','RSL' => 'RETURN SHORTAGE LOSSES' ]) ?>
 
     </div>
     <div class="col-md-12">
