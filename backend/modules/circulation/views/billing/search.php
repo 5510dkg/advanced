4d92a994b4da7c9ed892d8b680712870ded483a8
ubuntu <1124ritesh@gmail.com>
2016-04-06 16:29:46 +0530

    work on ui

--- backend/modules/circulation/views/billing/search.php
+++ backend/modules/circulation/views/billing/search.php
@@ -6,14 +6,17 @@ use dosamigos\datepicker\DatePicker;
 
 $this->title='Bill|Search';
 ?>
-<div class="row">
-    <div class="page-header">
-        <h1>Agency Billing |Search</h1>
-    </div>
-</div>
 
-<div class="row">
-    <div class="form-vertical">
+    <div class="box box-primary">
+        <div class="box-header with-border">
+        <h3 class="box-title">Bill Search</h3>
+        <div class="box-tools pull-right">
+            <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+        </div>
+    </div>
+    <div class="box-body">
+        <div class="row">
+            <div class="col-md-3">
    <?php $form = ActiveForm::begin(); ?>
         <?= $form->field($model, 'month')->widget(
     DatePicker::className(), [  
@@ -23,7 +26,8 @@ $this->title='Bill|Search';
                                 'viewMode'=> "months", 
                                 'minViewMode'=> "months",
         ]
-]);?>
+        ]);?></div>
+            <div class="col-md-3">
         <?= $form->field($model, 'name')->widget(Typeahead::classname(), [
                     'dataset' => [
                         [
@@ -34,12 +38,20 @@ $this->title='Bill|Search';
                     'pluginOptions' => ['highlight' => true],
                     'options' => ['placeholder' => 'Filter as you type ...'],
 ]); ?>
-        <?= $form->field($model, 'account_id') ?>
+            </div>
+            <div class="col-md-3">
+                <?= $form->field($model, 'account_id') ?>
+            </div>
+            <div class="col-md-3">
         <?= $form->field($model, 'mail_pincode')->label('Pin Code') ?>
+            </div>
+        </div>
+        
  
         <div class="form-group">
             <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
         </div>
     <?php ActiveForm::end(); ?>
-    </div>
+   
 </div>
+    </div>
