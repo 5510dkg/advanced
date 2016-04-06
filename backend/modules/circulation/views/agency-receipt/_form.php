d873f38930d8ad728b5f03b9109af2912fcab2ca
ubuntu <1124ritesh@gmail.com>
2016-04-06 17:51:37 +0530

    work on ui

--- backend/modules/circulation/views/agency-receipt/_form.php
+++ backend/modules/circulation/views/agency-receipt/_form.php
@@ -7,17 +7,21 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 
 ?>
-<div class="row">
-    <div class="page-header">
-        <h1>Agency Receipt</h1>
+
+<div class=" box box-default">
+    <div class="box-header with-border">
+        <h3 class="box-title">Agency Receipt </h3>
+        <div class="box-tools pull-right">
+            <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+        </div>
     </div>
-</div>
-
+    <div class="box-body">
 <div class="agency-receipt-form">
-
+    <div class="row">
+        <div class="col-md-4">
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'agency_id')->textInput(['value'=>$id,'type'=>'readonly']) ?>
+    <?= $form->field($model, 'agency_id')->textInput(['value'=>$id,'readonly'=>'readonly']) ?>
 
     <?= $form->field($model, 'rcpt_date')->widget(\dosamigos\datepicker\DatePicker::className(),
             [
@@ -26,21 +30,25 @@ use yii\widgets\ActiveForm;
                 'format' => 'yyyy-mm-dd'
                     ],
             ]) ?>
-
-    <?= $form->field($model, 'cr_amt')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-4">
+    <?= $form->field($model, 'cr_amt')->textInput(['maxlength' => '6']) ?>
 
     <?= $form->field($model, 'payment_mode')->dropDownList(\yii\helpers\ArrayHelper::map(backend\modules\settings\models\PaymentMode::find()->all(),'id','name'),['prompt'=>'Please Select Payment Mode']) ?>
+        </div>
+        <div class="col-md-4">
+    <?= $form->field($model, 'comment')->textarea(['rows' => 2]) ?>
 
-    <?= $form->field($model, 'comment')->textarea(['rows' => 6]) ?>
-
-    
-  
+    </div>
+    <div class="col-md-12">
 	<?php if (!Yii::$app->request->isAjax){ ?>
 	  	<div class="form-group">
 	        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
 	    </div>
 	<?php } ?>
-
+    </div>
     <?php ActiveForm::end(); ?>
     
+</div>
+    </div>
 </div>
