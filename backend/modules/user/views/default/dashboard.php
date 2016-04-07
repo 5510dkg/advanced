43e4e1f366589d7483894b6dec3c15ff470b3323
ubuntu <1124ritesh@gmail.com>
2016-04-07 11:24:56 +0530

    work on ui

--- backend/modules/user/views/default/dashboard.php
+++ backend/modules/user/views/default/dashboard.php
@@ -25,18 +25,18 @@ use yii\helpers\Html;
 <?php } ?>
 <?php  if(Yii::$app->user->can('generate-labels')){ ?>
 <div class="col-lg-4">
-                        <?=Html::a('Single Railway Label',['/circulation/railway-post-data'],['class'=>'btn btn-success btn-lg btn-block'])?> 
+                        <?=Html::a('Single Railway Label',['/circulation/railway-post-data/searchview'],['class'=>'btn btn-success btn-lg btn-block'])?> 
                 </div> 
 <?php } ?>
 <?php  if(Yii::$app->user->can('generate-labels')){ ?>
 <div class="col-lg-4">
-                        <?=Html::a('Single Ordinary Post Label',['/circulation/ordinary-post-data'],['class'=>'btn btn-success btn-lg btn-block'])?>  
+                        <?=Html::a('Single Ordinary Post Label',['/circulation/ordinary-post-data/searchview'],['class'=>'btn btn-success btn-lg btn-block'])?>  
                         
                 </div> 
 <?php } ?>
 <?php  if(Yii::$app->user->can('generate-labels')){ ?>
 <div class="col-lg-4">
-                        <?=Html::a('Single Registered Post Label',['/circulation/registered-post-data'],['class'=>'btn btn-success btn-lg btn-block'])?>  
+                        <?=Html::a('Single Registered Post Label',['/circulation/registered-post-data/searchview'],['class'=>'btn btn-success btn-lg btn-block'])?>  
                         
                 </div> 
 <?php } ?>
