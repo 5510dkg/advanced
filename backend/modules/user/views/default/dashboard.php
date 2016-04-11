b9eb7dcb27de62b42bfd21da9cb8f0023cf8b5f2
ubuntu <1124ritesh@gmail.com>
2016-04-11 12:51:37 +0530

    work on ui

--- backend/modules/user/views/default/dashboard.php
+++ backend/modules/user/views/default/dashboard.php
@@ -24,20 +24,3 @@ use yii\helpers\Html;
                 </div> 
 <?php } ?>
 <br/><br/><br/>
-<?php  if(Yii::$app->user->can('generate-labels')){ ?>
-<div class="col-lg-4">
-                        <?=Html::a('Single Railway Label',['/circulation/railway-post-data/searchview'],['class'=>'btn btn-success btn-lg btn-block'])?> 
-                </div> 
-<?php } ?>
-<?php  if(Yii::$app->user->can('generate-labels')){ ?>
-<div class="col-lg-4">
-                        <?=Html::a('Single Ordinary Post Label',['/circulation/ordinary-post-data/searchview'],['class'=>'btn btn-success btn-lg btn-block'])?>  
-                        
-                </div> 
-<?php } ?>
-<?php  if(Yii::$app->user->can('generate-labels')){ ?>
-<div class="col-lg-4">
-                        <?=Html::a('Single Registered Post Label',['/circulation/registered-post-data/searchview'],['class'=>'btn btn-success btn-lg btn-block'])?>  
-                        
-                </div> 
-<?php } ?>
