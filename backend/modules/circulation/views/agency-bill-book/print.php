5b4db7f7beeeec86d30f93c6c8574e8857efff2d
ubuntu <1124ritesh@gmail.com>
2016-04-15 12:18:15 +0530

    Merge origin/master

--- backend/modules/circulation/views/agency-bill-book/print.php
+++ backend/modules/circulation/views/agency-bill-book/print.php
@@ -14,8 +14,8 @@ $i=1;$r=1;?>
 
 
 	<div style="margin-left: 25px;margin-bottom: 40px;margin-right: -30px; width: 100%;float: left; height: 300px;">
-            <div style="height: 130px; width: 100%; border: 1px solid black;">
-                Space For Logo
+            <div style="height: 130px; width: 100%; border: none;">
+                <img src="images/header.jpg"/>
             </div>
             <div style="width: 100%;height: 15px;">
              Code:&nbsp;&nbsp;&nbsp;&nbsp;<?=$data[0]['account_id']?> 
