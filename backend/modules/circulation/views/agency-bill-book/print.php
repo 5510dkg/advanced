67fad9144f3f2b4d6023e8a49902c890d33ed791
ubuntu <1124ritesh@gmail.com>
2016-04-18 14:28:58 +0530

    Merge origin/master

--- backend/modules/circulation/views/agency-bill-book/print.php
+++ backend/modules/circulation/views/agency-bill-book/print.php
@@ -73,12 +73,6 @@ $i=1;$r=1;?>
                     ?>        
                 </tr> 
                 
-                
-                
-                
-                
-                
-                
                 <?php   endforeach;?>
                 <tr>
                     <th><strong>TOTAL SUPPLY</strong></th>
