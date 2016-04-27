408ffc65a46bc2b9234dc6ed1cfb44e35631ad51
ubuntu <1124ritesh@gmail.com>
2016-04-27 12:08:45 +0530

    Merge origin/master

--- backend/modules/circulation/controllers/AgencyBillBookController.php
+++ backend/modules/circulation/controllers/AgencyBillBookController.php
@@ -364,7 +364,6 @@ class AgencyBillBookController extends Controller
             $model = $this->findModel($pk);
             $model->delete();
         }
-        
         if($request->isAjax){
             /*
             *   Process for ajax request
